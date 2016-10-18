9e96a5559ea0589de8109edaceac254605140925
Justin Ricks <justin.rixx@gmail.com>
2016-10-18 11:18:26 -0600

    Flesh out some of the functions

--- neuralnet.py
+++ neuralnet.py
@@ -7,16 +7,59 @@ that type of a structure.
 import random
 
 
-def make_net(topology):
+class FFNN:
+    """ A multi-layer feed-forward neural net """
+
+    def __init__(self, topology, num_inputs, num_outputs):
+        self.topology = topology
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.layers = make_net(topology, num_inputs, num_outputs)
+
+
+def make_net(topology, num_inputs, num_outputs):
     """
     Makes a neural net (list of lists of lists) that is the desired size, and fills
     it with random values
     """
 
-    return []
+    # input layer
+    layers = [make_layer(num_inputs, topology[0])]
+
+    # hidden layers
+    for i in range(0, len(topology)):
+        layers.append(make_layer(len(layers[i - 1]), topology[i]))
+
+    # output layer
+    layers.append(make_layer(len(layers[-1]), num_outputs))
+
+    return layers
 
 
-def sp_crossover(net1, net2, topology):
+def make_layer(num_inputs, num_nodes):
+
+    layer = []
+
+    for i in range(0, num_nodes):
+        layer.append(make_node(num_inputs))
+
+    return layer
+
+
+def make_node(num_inputs):
+
+    node = []
+
+    for i in range(0, num_inputs):
+
+        # -1 <= weight < 1
+        weight = (random.random() - .5) * 2.0
+        node.append(weight)
+
+    return node
+
+
+def sp_crossover(net1, net2):
     """
     Single Point Crossover
     Picks a random point to split on, and the offspring get net1's DNA up to the
@@ -24,71 +67,120 @@ def sp_crossover(net1, net2, topology):
     that is the opposite of the first
     :param net1: The first parent
     :param net2: The second parent
-    :param topology: The topology of the neural net
     :return: two offspring, each the opposite of the other
     """
 
     total = 0
-    for layer in net1:
+    for layer in net1.layers:
         for node in layer:
             total += len(node)
 
     # the babies that will be returned
-    child1 = make_net(topology)
-    child2 = make_net(topology)
+    child1 = FFNN(net1.topology, net1.num_inputs, net1.num_outputs)
+    child2 = FFNN(net1.topology, net1.num_inputs, net1.num_outputs)
 
     split = random.randint(1, total - 1)
 
     index = 0
-    for i in range(0, len(net1)):
-        for j in range(0, len(net1[i])):
-            for k in range(0, len(net1[i][j])):
+    for i in range(0, len(net1.layers)):
+        for j in range(0, len(net1.layers[i])):
+            for k in range(0, len(net1.layers[i][j])):
                 if index < split:
-                    child1[i][j][k] = net1[i][j][k]
-                    child2[i][j][k] = net2[i][j][k]
+                    child1.layers[i][j][k] = net1.layers[i][j][k]
+                    child2.layers[i][j][k] = net2.layers[i][j][k]
                 else:
-                    child2[i][j][k] = net1[i][j][k]
-                    child1[i][j][k] = net2[i][j][k]
+                    child2.layers[i][j][k] = net1.layers[i][j][k]
+                    child1.layers[i][j][k] = net2.layers[i][j][k]
 
                 index += 1
 
     return child1, child2
 
 
-def u_crossover(net1, net2, topology):
+def u_crossover(net1, net2):
     """
     Uniform Crossover. For each weight, a parent is chosen at random, and the
     DNA is taken from that parent.
     :param net1: The first parent
     :param net2: The second parent
-    :param topology: The topology of the neural net
     :return: two offspring, each the opposite of the other
     """
 
-    child1 = make_net(topology)
-    child2 = make_net(topology)
+    child1 = FFNN(net1.topology, net1.num_inputs, net1.num_outputs)
+    child2 = FFNN(net1.topology, net1.num_inputs, net1.num_outputs)
 
-    for i in range(0, len(net1)):
-        for j in range(0, len(net1[i])):
-            for k in range(0, len(net1[i][j])):
+    for i in range(0, len(net1.layers)):
+        for j in range(0, len(net1.layers[i])):
+            for k in range(0, len(net1.layers[i][j])):
                 if random.randint(0, 1) == 0:
-                    child1[i][j][k] = net1[i][j][k]
-                    child2[i][j][k] = net2[i][j][k]
+                    child1.layers[i][j][k] = net1.layers[i][j][k]
+                    child2.layers[i][j][k] = net2.layers[i][j][k]
                 else:
-                    child2[i][j][k] = net1[i][j][k]
-                    child1[i][j][k] = net2[i][j][k]
+                    child2.layers[i][j][k] = net1.layers[i][j][k]
+                    child1.layers[i][j][k] = net2.layers[i][j][k]
 
     return child1, child2
 
 
 def to_file(filename, net):
     """
-    TODO write a neural net to a file
+    Write a neural net to a file
     """
-    return 0
+    if type(net) != FFNN:
+        raise TypeError('Cannot write an object that is not a FFNN to a file')
+
+    file = open(filename, "w")
+    file.truncate()
+
+    # num inputs, num outputs, num hidden layers
+    contents = net.num_inputs + " " + net.num_outputs + " " + len(net.layers)
+
+    # topology
+    for i in net.topology:
+        contents += " " + i
+
+    # weights
+    for i in range(0, len(net.layers)):
+        for j in range(0, len(net.layers[i])):
+            for weight in net.layers[i][j]:
+                contents += " " + weight
+
+    file.write(contents)
+    file.close()
 
 
 def mutate(net):
     """
-    TODO mutates a neural net in place
+    Mutates a neural net in place
+    :type net: The FFNN object or the list of layers. Either one
     """
+
+    if type(net) == FFNN:
+        network = net.layers
+    elif type(net) == list:
+        network = net
+    else:
+        raise TypeError('Net is not a FFNN or list')
+
+    # The chance for each weight to change is 1 / L where L is
+    #   the number of weights in the network
+    L = 0
+    for i in range(0, len(network)):
+        for j in range(0, len(network[i])):
+            L += len(network[i][j])
+
+    for i in range(0, len(network)):
+        for j in range(0, len(network[i])):
+            for k in range(0, len(network[i][j])):
+
+                coin = random.randint(0, L)
+
+                if coin == 0:
+                    # -5 <= r <= 5
+                    r = (random.random() - .5) * 5.0
+
+                    if network[i][j][k] != 0.0:
+                        network[i][j][k] *= r
+                    else:
+                        print("Weight is 0")
+                        network[i][j][k] += r
